d18580933c6446fe42e940872240c1aef0e06940
Christopher Wang <dev@christopher.wang>
2018-02-05 00:22:57 -0800

    Multisig changes, transactions are successful, but dropped
    The transaction is successful, but it is not shown on the dark
    explorer. It might not be implemented in core v1

--- src/Builders/TransactionBuilder.php
+++ src/Builders/TransactionBuilder.php
@@ -151,12 +151,12 @@ class TransactionBuilder
         return $transaction;
     }
 
-    public function createMultiSignature(string $secret, string $secondSecret, string $keysgroup, int $lifetime, int $min)
+    public function createMultiSignature(string $secret, string $secondSecret, array $keysgroup, int $lifetime, int $min)
     {
         $transaction = self::createEmptyTransaction();
         $transaction->type = TransactionType::MULTISIGNATURE;
         $transaction->amount = 0;
-        $transaction->fee = TransactionFee::MULTISIGNATURE;
+        $transaction->fee = (count($keysgroup) + 1) * TransactionFee::MULTISIGNATURE;
         $transaction->timestamp = self::getTimeSinceEpoch();
         $transaction->asset['multisignature'] = array(
             'min' => $min,
@@ -245,7 +245,7 @@ class TransactionBuilder
         {
             $out .= pack('C', $transaction->asset['multisignature']['min']);
             $out .= pack('C', $transaction->asset['multisignature']['lifetime']);
-            $out .= $transaction->asset['multisignature']['keysgroup'];
+            $out .= join('', $transaction->asset['multisignature']['keysgroup']);
         }
 
         if(!$skipSignature && $transaction->signature){
